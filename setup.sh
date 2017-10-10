5d32aac6c262e04421a1dfe9df193dde1359992b
Goetz Epperlein <goetz.epperlein@gmx.de>
2017-10-10 15:50:07 +0200

    Skip homebrew cask for travis testing

--- setup.sh
+++ setup.sh
@@ -2,13 +2,20 @@
 #
 # Script setup Ansible depending on OS
 
+# If test are run on travis avoid space intensive tasks
+if [ "$TRAVIS" == "true" ]; then
+  CMD_ANSIBLE='echo "\n### SPACE" && df -h && echo "\n### SETUP" && ansible-playbook -i "localhost," -c local ansible/site.yml --skip-tags "travis-do-not"'
+else
+  CMD_ANSIBLE='echo "\n### SETUP" && ansible-playbook -i "localhost," -c local ansible/site.yml'
+fi
+
 unameOut="$(uname -s)"
 case "${unameOut}" in
     Darwin*)
-      echo "Detected environment: Mac -> Install pip + ansible"
+      echo "\n### ENV\nOSX -> Install pip + ansible"
       sudo command -v pip >/dev/null 2>&1 || { sudo easy_install pip; }
       sudo command -v ansible >/dev/null 2>&1 || { sudo pip install ansible; }
-      ansible-playbook -i "localhost," -c local ansible/site.yml
+      eval $CMD_ANSIBLE
       ;;
 
     *)
