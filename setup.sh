11978d055134018354c229baa3eefe8eb671925f
Goetz Epperlein <goetz.epperlein@gmx.de>
2017-10-10 16:48:15 +0200

    FIX travis execution adding output

--- setup.sh
+++ setup.sh
@@ -4,15 +4,16 @@
 
 # If test are run on travis avoid space intensive tasks
 if [ "$TRAVIS" == "true" ]; then
-  CMD_ANSIBLE='echo "\n### SPACE" && df -h && echo "\n### SETUP" && ansible-playbook -i "localhost," -c local ansible/site.yml --skip-tags "travis-do-not"'
+  CMD_ANSIBLE='echo "\n### RUN df" && df -h && echo "\n### RUN ansible" && ansible-playbook -i "localhost," -c local ansible/site.yml --skip-tags "travis-do-not"'
 else
-  CMD_ANSIBLE='echo "\n### SETUP" && ansible-playbook -i "localhost," -c local ansible/site.yml'
+  CMD_ANSIBLE='echo "\n### RUN ansible" && ansible-playbook -i "localhost," -c local ansible/site.yml'
 fi
 
 unameOut="$(uname -s)"
 case "${unameOut}" in
     Darwin*)
-      echo "\n### ENV\nOSX -> Install pip + ansible"
+      echo "\n### ENV\nOSX"
+      echo "\n### INSTALL pip + ansible"
       sudo command -v pip >/dev/null 2>&1 || { sudo easy_install pip; }
       sudo command -v ansible >/dev/null 2>&1 || { sudo pip install ansible; }
       eval $CMD_ANSIBLE
