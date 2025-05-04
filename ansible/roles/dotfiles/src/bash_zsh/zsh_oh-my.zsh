9ffb957e0b7f12277dd8ae90a4bad513b04572b5
Goetz Epperlein <now@getgoetz.com>
2025-05-04 21:58:06 +0200

    refactor: Speed up zsh and oh-my-zsh - Add fzf - Only update compinit cache once a day - oh-my-zsh enhancements   > disable magic functions for speed up   > disable compfix for speed up   > add ZSH_AUTOSUGGEST_* config   > add fzf and config   > remove unnecessary comments

--- ansible/roles/dotfiles/src/bash_zsh/zsh_oh-my.zsh
+++ ansible/roles/dotfiles/src/bash_zsh/zsh_oh-my.zsh
@@ -4,9 +4,6 @@
 # ZSH OH-MY-ZSH #
 #################
 
-# If you come from bash you might have to change your $PATH.
-# export PATH=$HOME/bin:/usr/local/bin:$PATH
-
 # Path to your oh-my-zsh installation.
 export ZSH="${HOME}/.oh-my-zsh"
 
@@ -15,36 +12,14 @@ export ZSH="${HOME}/.oh-my-zsh"
 # See https://github.com/robbyrussell/oh-my-zsh/wiki/Themes
 ZSH_THEME="lony"
 
-# steeef
-# mira
-# mortalscumbag
-# gnzh
-# strug
-# fino
-# jonathan
-
-
-# Uncomment the following line to use case-sensitive completion.
-# CASE_SENSITIVE="true"
-
-# Uncomment the following line to use hyphen-insensitive completion. Case
-# sensitive completion must be off. _ and - will be interchangeable.
-# HYPHEN_INSENSITIVE="true"
-
 # Uncomment the following line to disable bi-weekly auto-update checks.
 DISABLE_AUTO_UPDATE="true"
 
-# Uncomment the following line to change how often to auto-update (in days).
-# export UPDATE_ZSH_DAYS=13
+# Uncomment the following line if pasting URLs and other text is messed up.
+DISABLE_MAGIC_FUNCTIONS="true"
 
-# Uncomment the following line to disable colors in ls.
-# DISABLE_LS_COLORS="true"
-
-# Uncomment the following line to disable auto-setting terminal title.
-# DISABLE_AUTO_TITLE="true"
-
-# Uncomment the following line to enable command auto-correction.
-# ENABLE_CORRECTION="true"
+# Disable warnings for insecure permissions on files used in command completion
+DISABLE_COMPFIX="true"
 
 # Uncomment the following line to display red dots whilst waiting for completion.
 COMPLETION_WAITING_DOTS="true"
@@ -53,11 +28,6 @@ COMPLETION_WAITING_DOTS="true"
 # under VCS as dirty. This makes repository status check for large repositories
 # much, much faster.
 DISABLE_UNTRACKED_FILES_DIRTY="true"
-
-# Uncomment the following line if you want to change the command execution time
-# stamp shown in the history command output.
-# The optional three formats: "mm/dd/yyyy"|"dd.mm.yyyy"|"yyyy-mm-dd"
-# HIST_STAMPS="mm/dd/yyyy"
 
 # Would you like to use another custom folder than $ZSH/custom?
 ZSH_CUSTOM="${HOME}/.zsh_oh-my-zsh"
@@ -70,61 +40,15 @@ plugins=(
 	z
 	zsh-autosuggestions
 	zsh-syntax-highlighting
+  fzf
 )
-
-	# aws
-	# brew
-	# common-aliases
-	# compleat
-	# dirhistory
-	# docker
-	# fasd
-	# git
-	# httpie
-	# history
-	# kubectl
-	# mvn
-	# node
-	# npm
-	# osx
-	# pip
-	# python
-	# sbt
-	# scala
-	# screen
-	# urltools
-	# vagrant
-	# vi-mode
-	# web-search
-	# wd
 
 source $ZSH/oh-my-zsh.sh
 
-# User configuration
+# see https://github.com/zsh-users/zsh-autosuggestions
+ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=8"
+ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE="20"
+ZSH_AUTOSUGGEST_USE_ASYNC=1
 
-# export MANPATH="/usr/local/man:$MANPATH"
-
-# You may need to manually set your language environment
-# export LANG=en_US.UTF-8
-
-# Preferred editor for local and remote sessions
-# if [[ -n $SSH_CONNECTION ]]; then
-#   export EDITOR='vim'
-# else
-#   export EDITOR='mvim'
-# fi
-
-# Compilation flags
-# export ARCHFLAGS="-arch x86_64"
-
-# ssh
-# export SSH_KEY_PATH="~/.ssh/rsa_id"
-
-# Set personal aliases, overriding those provided by oh-my-zsh libs,
-# plugins, and themes. Aliases can be placed here, though oh-my-zsh
-# users are encouraged to define aliases within the ZSH_CUSTOM folder.
-# For a full list of active aliases, run `alias`.
-#
-# Example aliases
-# alias zshconfig="mate ~/.zshrc"
-# alias ohmyzsh="mate ~/.oh-my-zsh"
+# see https://github.com/junegunn/fzf
+FZF_DEFAULT_OPTS='--height 40% --layout reverse'
